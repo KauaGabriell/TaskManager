d4b85cdf0c772897d46dbf3a9356e3cc4f24f3bd
KauaGabriell <kauagabriell64@gmail.com>
2026-05-16 01:43:57 -0300

    build(deps): add bcrypt

---
+++ src/utils/hashAndVerifyPassword.ts
@@ -0,0 +1,16 @@
+import bcrypt from 'bcrypt';
+import { AppError } from './AppError.js';
+
+export async function hashPassword(password: string) {
+  const salt = await bcrypt.genSalt(10);
+
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  return hashedPassword;
+}
+
+export async function verifyPassword(password: string, userPassword: string) {
+  const passwordMatch = await bcrypt.compare(password, userPassword);
+
+  if (!passwordMatch) throw new AppError('Invalid Credentials', 401);
+}
